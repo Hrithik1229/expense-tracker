9773d94278625cb745dc60eba822eef44946c5ef
Hrithik k <hrithik2538@gmail.com>
2025-12-09 11:30:48 +0530

    Add database schema

---
+++ Schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE expense_tracker;
+
+USE expense_tracker;
+
+CREATE TABLE expenses (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    category VARCHAR(50),
+    expense_date DATE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
